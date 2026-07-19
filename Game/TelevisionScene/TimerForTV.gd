7c8968749e36621fc53793970c0dc6c8c690f89d
João Lucas Silva Machado <joaolucas.silva@estudante.ufjf.br>
2026-07-19 13:11:40 -0300

    Faz a TV voltar no tempo tambem

--- Game/TelevisionScene/TimerForTV.gd
+++ Game/TelevisionScene/TimerForTV.gd
@@ -6,3 +6,7 @@ extends Label
 		time_value = value
 		var total = maxf(value, 0.0)
 		text = "%02d.%02d" % [int(total), int(total * 100) % 100]
+
+func _process(_delta):
+	var elapsed = TimeLoop.current_tick / float(Engine.physics_ticks_per_second)
+	time_value = TimeLoop.loop_duration_seconds - elapsed
