365ed057e2e3f701f7d0a2252df0da407a11de16
João Pedro Miranda Salim <joaopedromirandasalim@gmail.com>
2026-07-19 15:55:26 -0300

    Corrige minigame de sinwave para mostrar quando está ativo

--- Game/Interact/tv_sinwave.gd
+++ Game/Interact/tv_sinwave.gd
@@ -3,8 +3,14 @@ extends Interactable
 @export_group("Sin config")
 @export var max_amp = 0.5
 @export var max_freq = 40.0
-var actual_freq = 38.0
-var actual_amp = 0.12
+var actual_freq = 38.0:
+	set(value):
+		actual_freq = clampf(value, -max_freq, max_freq)
+		screen_material.set_shader_parameter("green_freq", actual_freq)
+var actual_amp = 0.12:
+	set(value):
+		actual_amp = clampf(value, -max_amp, max_amp)
+		screen_material.set_shader_parameter("green_amp", actual_amp)
 
 var red_freq : float
 var red_amp : float
@@ -31,12 +37,17 @@ func uses_mouse_drag():
 func on_drag(_agent, _drag):
 	speed = _drag * mouse_sensitivity * 0.0005
 	var change : Vector2 = Vector2(max_freq * speed.x, max_amp * speed.y)
-	actual_freq = clampf(actual_freq + change.x, -max_freq, max_freq)
-	actual_amp = clampf(actual_amp + change.y, -max_amp, max_amp)
-	screen_material.set_shader_parameter("green_freq", actual_freq)
-	screen_material.set_shader_parameter("green_amp", actual_amp)
+	actual_freq = actual_freq + change.x
+	actual_amp = actual_amp + change.y
+	if is_active():
+		screen_material.set_shader_parameter("hasSolved", true)
 
 func is_active():
 	var dif_freq = abs(actual_freq) - red_freq
 	var dif_amp = abs(actual_amp) - red_amp
 	return dif_freq < 1.0 and dif_amp < 0.05 
+
+func reset_state():
+	screen_material.set_shader_parameter("hasSolved", false)
+	actual_amp = 0.2
+	actual_freq = 30
